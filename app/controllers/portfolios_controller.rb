56f56cdcf3d6a9064eb63a13f609622c5b973a67
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-22 16:39:13 +0000

    added new and edit portfolio buttons

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,16 +5,31 @@ class PortfoliosController < ApplicationController
     def new
         @port = Portfolio.new
     end
-     def create
+   def create
     @port = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
+     respond_to do |format|
+       if @port.save
+         format.html { redirect_to portfolios_path, notice: 'Bat Portfolio posted' }
+       else
+         format.html { render :new }
+       end
+     end
+   end
+     
+     def edit
+         @port = Portfolio.find(params[:id])
+     end
+     
+     def update
+          @port = Portfolio.find(params[:id])
+         
     respond_to do |format|
-      if @port.save
-        format.html { redirect_to portfolios_path, notice: 'Bat Portfolio posted' }
+      if @port.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Bat Portfolio updated.' }
       else
-        format.html { render :new }
+        format.html { render :edit }
       end
     end
      end
-     
 end
